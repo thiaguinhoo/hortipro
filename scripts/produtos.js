023b022ababcc0986ecff6d9e1c3af0c31c3dd8b
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-05-25 09:10:30 -0300

    Array de produtos. Refatoracao da lista de produtos

---
+++ scripts/produtos.js
@@ -0,0 +1,63 @@
+const produtos = [
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Banana",
+    preco: "R$ 6,00",
+    estoque: 12,
+    id: 1,
+  },
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Pera",
+    preco: "R$ 10,00",
+    estoque: 10,
+    id: 2,
+  },
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Uva",
+    preco: "R$ 10,00",
+    estoque: 10,
+    id: 3,
+  },
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Laranja",
+    preco: "R$ 10,00",
+    estoque: 10,
+    id: 4,
+  },
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Chocolate",
+    preco: "R$ 10,00",
+    estoque: 10,
+    id: 5,
+  },
+  {
+    imagem:
+      "https://conteudo.imguol.com.br/c/entretenimento/24/2020/09/15/banana-1600197261350_v2_4x3.jpg",
+    nome: "Morango",
+    preco: "R$ 10,00",
+    estoque: 10,
+    id: 6,
+  },
+];
+
+// const listaProdutos = document.querySelector(".lista_produtos-categoria");
+// const produtoClone = document
+//   .querySelector(".lista_produtos-produto")
+//   .cloneNode(true);
+// const produtoImagem = document.querySelector(".lista_produtos-imagem");
+// const produtoInfo = document.querySelector(".lista_produtos-produto-info");
+
+// window.addEventListener("load", () => {
+//   produtos.forEach((produtoClone) => {
+//     listaProdutos.appendChild(produtoClone);
+//   });
+// });
