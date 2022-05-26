66d36e511405f2954791f3eb0d1363cde3708573
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-05-26 12:58:49 -0300

    Fix/Botao de scroll dos produtos da horta

--- scripts/arrow.js
+++ scripts/arrow.js
@@ -1,23 +1,22 @@
 var scrolling = false;
-let step = 400
+let step = 400;
 
-const scrollLeft = document.querySelector(".scroll_left")
-const scrollRight = document.querySelector(".scroll_right")
-const listaHorta = document.querySelector(".lista_produtos-categoria")
-
+const scrollLeft = document.querySelector(".scroll_left");
+const scrollRight = document.querySelector(".scroll_right");
+const listaHorta = document.querySelector("#horta-produtos");
 
 scrollLeft.addEventListener("click", (event) => {
-  event.preventDefault()
+  event.preventDefault();
   listaHorta.scrollBy({
     left: -step,
-    behavior: "smooth"
-})
-})
+    behavior: "smooth",
+  });
+});
 
 scrollRight.addEventListener("click", (event) => {
-  event.preventDefault()
+  event.preventDefault();
   listaHorta.scrollBy({
     left: step,
-    behavior: "smooth"
-})
-})
+    behavior: "smooth",
+  });
+});
